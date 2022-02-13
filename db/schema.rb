79c8890491e5c75036ecdda527a2ca1a2c09e558
Shimaxx186 <shunbusiness.8621@gmail.com>
2022-02-13 22:45:43 +0000

    [add]投稿機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_13_095237) do
+ActiveRecord::Schema.define(version: 2022_02_13_212948) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "image_id"
+    t.text "contents"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
