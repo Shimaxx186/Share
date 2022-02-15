5f08d6910316d18831cfa472923d5a2523450567
Shimaxx186 <shunbusiness.8621@gmail.com>
2022-02-15 05:12:31 +0000

    [add]フォロー、フォロワー機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  
   devise_for :users
+  
   root to: 'homes#top'
+  
   resources :users, only: [:index, :show, :edit, :update]
+  post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォロー外す
+
   resources :posts do
     resources :comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
+  
 end
