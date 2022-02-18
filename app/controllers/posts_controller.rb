90a6e1e1b5093038aa9f542f8f9f2f8f15968546
Shimaxx186 <shunbusiness.8621@gmail.com>
2022-02-18 06:07:03 +0000

    [update]ajax

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
-    # @posts = Post.page(params[:page]).reverse_order
+    @posts = Post.order(created_at: :desc).page(params[:page]).per(3)
   end
 
   def show
@@ -11,6 +11,7 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def new
@@ -25,6 +26,9 @@ class PostsController < ApplicationController
   end
 
   def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to posts_path
   end
 
   def destroy
