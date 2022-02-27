f6d7c5e9ec909d8218e1e348d0e5ea787dfb2a4a
Shimaxx186 <shunbusiness.8621@gmail.com>
2022-02-27 11:56:12 +0000

    [fix]mypage

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,6 @@
 class Users::SessionsController < Devise::SessionsController
   def guest_sign_in
-    user = User.find_or_create_by!(name: "ゲスト", email: 'guest@example.com') do |user|
+    user = User.find_or_create_by!(name: "guestuser", email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
     end
     sign_in user
